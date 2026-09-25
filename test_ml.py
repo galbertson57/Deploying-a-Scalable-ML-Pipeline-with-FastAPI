dedb43bea38c6039daae4884e2f3f6e521aa9db2
Grace <grace.albertson.snyder1@gmail.com>
2026-09-25 17:22:39 +0000

    Taking a break but added unit tests to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,55 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+import pandas as pd
+import pytest
+from sklearn.ensemble import RandomForestClassifier
+from ml.data import process_data
+from ml.model import compute_model_metrics, inference, train_model
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
-    # add description for the first test
+    Test train_model returns fitted Random Forest Classifier
     """
-    # Your code here
-    pass
+    X_train = np.array([[1,2],[3,4],[5,6],[7,8]])
+    y_train = np.array([0,1,0,1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
     """
-    # add description for the second test
+    Test compute_model_metrics returns expected precision, recall, and
+    F1 values on set of labels/predictions
     """
-    # Your code here
-    pass
+    y = np.array([1,0,1,1,0])
+    preds = np.array([1,0,1,0,0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert precision == 1.0
+    assert recall == pytest.approx(2/3)
+    assert fbeta == pytest.approx(0.8)
 
 
 # TODO: implement the third test. Change the function name and input as needed
 def test_three():
     """
-    # add description for the third test
+    Test inference returns predictions of correct type/shape, matching number
+    of input rows.
     """
-    # Your code here
-    pass
+    data = pd.DataFrame({
+        "age":[25, 40, 33, 52],
+        "workclass":["Private", "Self-emp", "Private", "Gov"],
+        "salary":["<=50K", ">50K", "<=50K", ">50K"]
+    })
+    cat_features = ["workclass"]
+    X, y, encoder, lb = process_data(
+        data,
+        categorical_features = cat_features,
+        label = "salary",
+        training = True
+    )
+    model = train_model(X,y)
+    preds = inference(model, X)
+    assert isinstance(preds, np.ndarray)
+    assert preds.shape[0] == X.shape[0]
