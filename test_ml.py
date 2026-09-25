5ffa638e9f23c038b50d26b8117393fd78f281af
grace <grace.albertson.snyder1@gmail.com>
2026-09-25 21:46:23 +0000

    fix spacing issues in flake8

--- test_ml.py
+++ test_ml.py
@@ -1,18 +1,18 @@
 import pytest
 import numpy as np
 import pandas as pd
-import pytest
 from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
 from ml.model import compute_model_metrics, inference, train_model
+
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
     Test train_model returns fitted Random Forest Classifier
     """
-    X_train = np.array([[1,2],[3,4],[5,6],[7,8]])
-    y_train = np.array([0,1,0,1])
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
     model = train_model(X_train, y_train)
     assert isinstance(model, RandomForestClassifier)
 
@@ -23,11 +23,11 @@ def test_two():
     Test compute_model_metrics returns expected precision, recall, and
     F1 values on set of labels/predictions
     """
-    y = np.array([1,0,1,1,0])
-    preds = np.array([1,0,1,0,0])
+    y = np.array([1, 0, 1, 1, 0])
+    preds = np.array([1, 0, 1, 0, 0])
     precision, recall, fbeta = compute_model_metrics(y, preds)
     assert precision == 1.0
-    assert recall == pytest.approx(2/3)
+    assert recall == pytest.approx(2 / 3)
     assert fbeta == pytest.approx(0.8)
 
 
@@ -38,18 +38,18 @@ def test_three():
     of input rows.
     """
     data = pd.DataFrame({
-        "age":[25, 40, 33, 52],
-        "workclass":["Private", "Self-emp", "Private", "Gov"],
-        "salary":["<=50K", ">50K", "<=50K", ">50K"]
+        "age": [25, 40, 33, 52],
+        "workclass": ["Private", "Self-emp", "Private", "Gov"],
+        "salary": ["<=50K", ">50K", "<=50K", ">50K"]
     })
     cat_features = ["workclass"]
     X, y, encoder, lb = process_data(
         data,
-        categorical_features = cat_features,
-        label = "salary",
-        training = True
+        categorical_features=cat_features,
+        label="salary",
+        training=True
     )
-    model = train_model(X,y)
+    model = train_model(X, y)
     preds = inference(model, X)
     assert isinstance(preds, np.ndarray)
     assert preds.shape[0] == X.shape[0]
